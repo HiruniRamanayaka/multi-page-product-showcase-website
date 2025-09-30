97ab8cb5974fb6cc50a4837ff624662bfd81d978
Hiruni Ramanayaka <hiruniramanayaka9@gmail.com>
2025-10-01 01:20:03 +0530

    Modified App.jsx, App.css, index.css and main.jsx

---
+++ my-product-site/src/App.jsx
@@ -0,0 +1,30 @@
+import { Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import About from './pages/About';
+import Products from './pages/Products';
+import Gallery from './pages/Gallery';
+import Contact from './pages/Contact';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/gallery" element={<Gallery />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </main>
+      <Navbar />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
